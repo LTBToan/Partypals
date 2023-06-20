e83c3c8f6aa0f8dc941d3627c4d49deb7ff1b2bf
Trinh Binh Minh <minhtbse151276@fpt.edu.vn>
2023-06-20 22:40:15 +0700

    UpdateContacts

--- models/contact.js
+++ models/contact.js
@@ -1,6 +1,14 @@
 const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Schema;
 const contactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
   title: {
     type: String,
     required: true,
